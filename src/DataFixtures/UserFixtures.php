1b9790f4a0442daac050636299625cb2ec14410c
Antoine GREUZARD <antoin.greuzard@gmail.com>
2025-03-25 12:25:03 +0100

    feat: add User Fixture and migration

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(
+        private readonly UserPasswordHasherInterface $hasher
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setEmail('test@example.com');
+        $user->setRoles(['ROLE_USER']);
+
+        $hashedPassword = $this->hasher->hashPassword($user, 'testpass123');
+        $user->setPassword($hashedPassword);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
