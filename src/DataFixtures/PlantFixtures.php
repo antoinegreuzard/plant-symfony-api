384696464cdd57eb332a98cde8cdb0b713a7500e
Antoine GREUZARD <antoin.greuzard@gmail.com>
2025-04-10 10:41:50 +0200

    fix: date time for update

--- src/DataFixtures/PlantFixtures.php
+++ src/DataFixtures/PlantFixtures.php
@@ -7,6 +7,7 @@ namespace App\DataFixtures;
 use App\Entity\Plant;
 use App\Entity\PlantPhoto;
 use App\Entity\User;
+use DateTime;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -35,17 +36,17 @@ class PlantFixtures extends Fixture implements DependentFixtureInterface
                 ->setName("Plante $i")
                 ->setVariety("Variété $i")
                 ->setPlantType($types[array_rand($types)])
-                ->setPurchaseDate(new DateTimeImmutable('-'.rand(0, 365).' days'))
+                ->setPurchaseDate(new DateTime('-'.rand(0, 365).' days'))
                 ->setLocation("Emplacement $i")
                 ->setDescription("Ceci est une description pour la plante $i.")
                 ->setWateringFrequency(rand(3, 10))
                 ->setFertilizingFrequency(rand(20, 60))
                 ->setRepottingFrequency(rand(180, 400))
                 ->setPruningFrequency(rand(60, 180))
-                ->setLastWatering(new DateTimeImmutable('-'.rand(1, 7).' days'))
-                ->setLastFertilizing(new DateTimeImmutable('-'.rand(1, 30).' days'))
-                ->setLastRepotting(new DateTimeImmutable('-'.rand(1, 365).' days'))
-                ->setLastPruning(new DateTimeImmutable('-'.rand(1, 90).' days'))
+                ->setLastWatering(new DateTime('-'.rand(1, 7).' days'))
+                ->setLastFertilizing(new DateTime('-'.rand(1, 30).' days'))
+                ->setLastRepotting(new DateTime('-'.rand(1, 365).' days'))
+                ->setLastPruning(new DateTime('-'.rand(1, 90).' days'))
                 ->setSunlightLevel($sunlights[array_rand($sunlights)])
                 ->setHumidityLevel($humidities[array_rand($humidities)])
                 ->setTemperature(rand(15, 28));
